9e74a40a80c91d11bdee07e39b5145d6b2e4c10a
graghtb <epicfox224@gmail.com>
2021-08-13 10:16:17 +1000

    Should work on linux now

--- scanner_nocolorama.py
+++ scanner_nocolorama.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+#should work on linux now
 #a huge thank you and shoutout to stackoverflow my lord and savior <3
 #feel free to clone this and use it during your CTF adventures.
 
 import socket, sys, threading
 from os import system, name
-
 global verbosity 
 verbosity = 0
 def clear():
@@ -28,8 +28,8 @@ def beginScan(ip_addr,port_start,port_end,multihost_flag):
         for port in range(int(port_start), int(port_end) + 1):
             t = threading.Thread(target=portscan,args=(ip_addr,port)) 
             t.start() 
-        if verbosity == 1:
-            print(f"[+] Scan on host {ip_addr} complete.")        
+        
+        print(f"[+] Scan on host {ip_addr} complete.")        
     elif multihost_flag == 1:
         for host_portion in range (1,255):
             ip_addr_m = ip_addr + "." + str(host_portion)
@@ -39,15 +39,16 @@ def beginScan(ip_addr,port_start,port_end,multihost_flag):
             for port in range(int(port_start), int(port_end) + 1):
                 t = threading.Thread(target=portscan,args=(ip_addr_m,port)) 
                 t.start() 
-            if verbosity == 1:    
-                print(f"[+] Scan on host {ip_addr_m} complete.") 
+                
+            print(f"[+] Scan on host {ip_addr_m} complete.") 
 
 if __name__ == '__main__':
     if sys.argv.__contains__("-v"):
         verbosity = 1
-    if sys.argv.__contains__('-h'):
+    if sys.argv.__contains__("-h"):
             clear()
             print(f"[+] Usage:\n[*]  Verbosity: -v\n[*]  Single Host Scan: -s\n [-]  e.g ./scanner.py <ip> <port_start> <port_end> -s\n [-]  e.g ./scanner.py 10.1.1.1 1 65535 -s\n[*]  Scan Multiple Hosts: -m\n [-]  e.g ./scanner.py <ip up to host portion> <port_start> <port_end> -m\n [-] e.g ./scanner.py 10.1.1 1 65535 -m\n[*]  Help: -h\n [-] e.g ./scanner.py -h\n")
+            quit()
     if len(sys.argv) <= 6:
         if sys.argv.__contains__("-s"):
             multihost_flag = 0
